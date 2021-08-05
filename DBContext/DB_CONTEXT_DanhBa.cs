ef0d3cfaa5914f87523066de45ced815f32fe510
Kiều Nguyễn <beu_csnv@outlook.com>
2021-08-05 21:49:02 +0700

    ádasd

--- DBContext/DB_CONTEXT_DanhBa.cs
+++ DBContext/DB_CONTEXT_DanhBa.cs
@@ -1,16 +1,21 @@
 ﻿using ASSIGMENT_Danh_Ba.Models;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace ASSIGMENT_Danh_Ba.DBContext
 {
-    public class DB_CONTEXT_DanhBa:DbContext
+    public class DB_CONTEXT_DanhBa : DbContext
     {
-        public  DbSet<Nguoi> Nguois { get; set; }
-        public  DbSet<DanhBa> DanhBas { get; set; }
+        public DbSet<Nguoi> Nguois { get; set; }
+        public DbSet<DanhBa> DanhBas { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
-                @"Data Source=DESKTOP-NVB7S6L; Initial Catalog=ASSIGRMENT_WINFORM; Persist Security Info=True; User ID=kieu96;Password=123");
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                 @"Data Source=DESKTOP-NVB7S6L; Initial Catalog=ASSIGRMENT_WINFORM; Persist Security Info=True; User ID=kieu96;Password=123");
+            }
+
         }
     }
 }
